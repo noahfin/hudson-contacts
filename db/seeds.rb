9d127ed25babf8c5280bce8409d0f36eed79253d
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-20 21:53:27 -0400

    Add Faker data for goups and contacts

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+
+
+Group.destroy_all
+Contact.destroy_all
+
+group_ids = []
+group_ids << Group.create(name: 'Ulster County').id
+group_ids << Group.create(name: 'Dutchess County').id
+group_ids << Group.create(name: 'Westchester County').id
+
+
+p "#{group_ids.count} created"
+
+group_count = group_ids.length
+number_of_contacts = 20
+contacts = []
+
+number_of_contacts.times do |i|
+  new_contact = {
+
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    company: Faker::Company.name,
+    phone: Faker::PhoneNumber.cell_phone,
+    cell: Faker::PhoneNumber.cell_phone,
+    address: "#{Faker::Address.street_address} #{Faker::Address.zip} #{Faker::Address.city } #{Faker::Address.state}",
+    group_id: group_ids[Random.rand(0...group_count)]
+  }
+  contacts.push(new_contact)
+
+end
+
+
+p "#{number_of_contacts} created"

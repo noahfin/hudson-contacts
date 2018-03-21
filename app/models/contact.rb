74ade379f29fa7a2c3f6bb0d4f5a08db8ae6232d
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-21 13:25:09 -0400

    Add gravatar

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
+  require 'digest/md5'
+
   belongs_to :group
+
+  def gravater
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}"
+
+  end
 end

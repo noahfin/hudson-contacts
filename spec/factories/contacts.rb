5d86d20db63a0b45c23fa35fcae358eb10b32f74
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-20 21:12:03 -0400

    Add documation page

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :contact do
+    name "MyString"
+    company "MyString"
+    email "MyString"
+    phon "MyString"
+    address "MyString"
+    group nil
+  end
+end

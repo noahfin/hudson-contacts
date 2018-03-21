372b92fe704daaece57a4f04e315ba542955ebf5
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-21 18:34:51 -0400

    ADD CONTACTS BY GROUP

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,8 @@
 class ContactsController < ApplicationController
   def index
+    if params[:group_id] &&  !params[:group_id]).page(params[:page])
+    else
     @contacts = Contact.page(params[:page])
   end
+ end
 end

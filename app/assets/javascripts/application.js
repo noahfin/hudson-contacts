01902f7e849f5df5fb9568934ce731ad3e1ef47e
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-21 12:29:39 -0400

    a render function for partial contacts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,25 @@
 //= require_tree .
 
 
-jquery.jQuery(document).ready(function($) {
-  ('.left').hover(
-  function () {
-    $('#lower-nv-links').attr("style", "display:none");
-  },
-  function () {
-    $('#lower-nv-links').attr("style", "display:block");
-  }
-);
+jQuery(document).ready(function($) {
+  var fixmeTop = $('.fixme').offset().top;       // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.sidenavbar').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
+    } else {                                   // apply position: static
+        $('.sidenavbar').css({                      // if you scroll above it
+            position: 'static'
+        });
+    }
+
+});
+
 });

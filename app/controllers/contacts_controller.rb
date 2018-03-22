e327b64240f7131c76579098cb948b78310f9c97
Noah Finnerman <noahfinnerman@gmail.com>
2018-03-21 21:14:39 -0400

    Add styles and layout for contacts add form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,4 +7,13 @@ class ContactsController < ApplicationController
     @contacts = Contact.page(params[:page])
   end
  end
+
+ def new
+  if params[:group_id] && !params[:group_id].empty?
+      # @contacts =Contact.where(group_id: params[:group_id]).page(params[:page])
+      @contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+    else
+    @contacts = Contact.page(params[:page])
+  end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
-
-
-  get  '/about', to: 'pages#about'
   devise_for :users
   resources :users
 
   authenticated do
     root :to => 'contacts#index'
     get 'contacts/index', as: 'contacts'
+    get 'contacts/new', as: 'new_contact'
   end
 
   root to: 'visitors#index'
+  get  '/about', to: 'pages#about'
+
 end
